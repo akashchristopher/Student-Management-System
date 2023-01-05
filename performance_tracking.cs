62b0b890d2e5477807c056b5175a13f217e99ec4
akashchristopher <77973710+akashchristopher@users.noreply.github.com>
2023-01-05 16:30:47 +0530

    Add files via upload
    performance_tracking

---
+++ performance_tracking.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class performance_tracking
+    {
+        string _quarterly;
+
+        public string Quarterly
+        {
+            get { return _quarterly; }
+            set { _quarterly = value; }
+        }
+
+        string _half_yearly;
+        public string Half_yearly
+        {
+            get { return _half_yearly; }
+            set { _half_yearly = value; }
+        }
+
+        string _annaual;
+
+        public string Annaual
+        {
+            get { return _annaual; }
+            set { _annaual = value; }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("quarterly performance : {0} \t  half_yearly performance : {1} \t annual performance : {2}",this._quarterly,this._half_yearly,this._annaual);
+        }
+    }
+}
