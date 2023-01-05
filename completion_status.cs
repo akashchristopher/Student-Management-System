3d4f848d586912c3cad237936b5cbe6826a4ac7c
akashchristopher <akashchristopher4@gmail.com>
2023-01-05 16:18:13 +0530

    completion_status

---
+++ completion_status.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class completion_status
+    {
+        string _iscomplete;
+        public string Iscomplete
+        {
+            get { return _iscomplete; }
+            set { _iscomplete = value; }
+        }
+        public override string ToString()
+        {
+            return string.Format("course complete status : {0}",this.Iscomplete);
+        }
+    }
+}
